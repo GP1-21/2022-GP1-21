830346c1ff8257caa590160f22a3ca42b054f801
Xcoder77 <in22lala@hotmail.com>
2023-04-01 21:01:19 +0300

    Some Styling Changes

--- lib/Screens/city_Screen.dart
+++ lib/Screens/city_Screen.dart
@@ -6,11 +6,9 @@ import 'package:huna_ksa/Components/common_Functions.dart';
 import 'package:huna_ksa/Components/constants.dart';
 import 'package:huna_ksa/Components/session.dart' as session;
 
-
-
 final _firestore = FirebaseFirestore.instance;
-class CityScreen extends StatefulWidget {
 
+class CityScreen extends StatefulWidget {
   @override
   State<CityScreen> createState() => _CityScreenState();
 }
@@ -23,43 +21,39 @@ class _CityScreenState extends State<CityScreen> {
     CityCard(imagePath: "images/Al-ola.png", cityName: "Al-Ula"),
     CityCard(imagePath: "images/Alkhobar.png", cityName: "Alkhobar")
   ];
-  saveCity(String city)
-  async {
-    await _firestore
-        .collection('userData')
-        .doc(session.email)
-        .update(
+  saveCity(String city) async {
+    await _firestore.collection('userData').doc(session.email).update(
       {
         'city': city,
       },
     ).then((value) {
       setState(() {
-        session.city=city;
+        session.city = city;
       });
-      tost(context, city+" is selected.");
       pushAndRemove(context, MainScreen());
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: ListView.builder(
-        //scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.only(left: 30, right:30,bottom: 10,top: 10),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0),
+        child: ListView.builder(
+          //scrollDirection: Axis.horizontal,
+          padding:
+              const EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 10),
           itemCount: cards.length,
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
-            onTap:()
-    {
-      saveCity(cards.elementAt(index).cityName);
-    },
-    child: cards.elementAt(index));
+                onTap: () {
+                  saveCity(cards.elementAt(index).cityName);
+                },
+                child: cards.elementAt(index));
           }
-        //children: orderBubbles,
+          //children: orderBubbles,
 
-
-      ),
-    );
-  }
-}
+          ),
+    ),
+  );

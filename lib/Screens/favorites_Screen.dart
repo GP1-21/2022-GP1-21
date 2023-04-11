aa5bd5258c7f101094fc2c576f625606f9280f4a
Xcoder77 <in22lala@hotmail.com>
2023-04-11 06:43:01 +0300

    Correct some mistake

--- lib/Screens/favorites_Screen.dart
+++ lib/Screens/favorites_Screen.dart
@@ -1,13 +1,14 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:huna_ksa/Components/constants.dart';
 import 'package:huna_ksa/Components/session.dart' as session;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 import 'package:huna_ksa/Screens/placeDetail_Screen.dart';
 import 'package:huna_ksa/Widgets/favourite_Card.dart';
-
 import '../Components/common_Functions.dart';
-final _firestore = FirebaseFirestore.instance;
 
+final _firestore = FirebaseFirestore.instance;
 
 class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
@@ -21,25 +22,26 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     showAlertDialog(context, () async {
       await _firestore
           .collection('favouritePlace')
-          .doc(session.email+place).delete().then((value) {
+          .doc(session.email + place)
+          .delete()
+          .then((value) {
         setState(() {
           session.favourite.remove(place);
           print(session.favourite);
         });
-Navigator.pop(context);
+        Navigator.pop(context);
       });
-    }, "Delete", "Are you sure you want to delete place?");
-
-
+    }, "Delete", "Are you sure you want to delete the place?");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body:  StreamBuilder<QuerySnapshot>(
-          stream: _firestore.collection('favouritePlace')
-              .where("email",isEqualTo: session.email)
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _firestore
+              .collection('favouritePlace')
+              .where("email", isEqualTo: session.email)
               .snapshots(),
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
@@ -52,26 +54,26 @@ Navigator.pop(context);
             }
             final placeData = snapshot.data?.docs;
 
-
             return ListView.builder(
-              itemCount: placeData?.length,
-                padding: EdgeInsets.all(20),
-                itemBuilder: (BuildContext context,int index){
-
-                  return GestureDetector(onTap:() {
-                    push(context, PlaceDetailScreen(place: placeData?.elementAt(index).get("place"), imageURL: placeData?.elementAt(index).get(
-                        "image")));
-                  },
-                      child: FavouriteCard(imagePath:placeData?.elementAt(
-                    index).get("image"),placeName: placeData?.elementAt(index).get("place"),delete:deleteFavourite,));
-                }
-
-
-            );
-          }
-
-      ),
-
+                itemCount: placeData.length,
+                padding: const EdgeInsets.all(20),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                      onTap: () {
+                        push(
+                            context,
+                            PlaceDetailScreen(
+                                place: placeData?.elementAt(index).get("place"),
+                                imageURL:
+                                    placeData?.elementAt(index).get("image")));
+                      },
+                      child: FavouriteCard(
+                        imagePath: placeData?.elementAt(index).get("image"),
+                        placeName: placeData?.elementAt(index).get("place"),
+                        delete: deleteFavourite,
+                      ));
+                });
+          }),
     );
   }
 }

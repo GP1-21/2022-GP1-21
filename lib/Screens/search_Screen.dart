becd0370362e5aa2b971f0a41b98faed118d03b5
GP1-21 <113027700+GP1-21@users.noreply.github.com>
2023-04-14 20:39:32 +0300

    Update search_Screen.dart

--- lib/Screens/search_Screen.dart
+++ lib/Screens/search_Screen.dart
@@ -4,18 +4,23 @@ import 'package:huna_ksa/Components/constants.dart';
 import 'package:huna_ksa/Components/session.dart' as session;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:huna_ksa/Screens/placeDetail_Screen.dart';
-
 import '../Widgets/search_Card.dart';
+
+//connect to the database in firebase
 final _firestore = FirebaseFirestore.instance;
+
+//StatefulWidget describes part of the user interface
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
+  //state read synchronously when the widget is built, might change during the lifetime of the widget
   @override
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController searchController=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +41,8 @@ class _SearchScreenState extends State<SearchScreen> {
     setState(() {
     });
     },
+
+    //search bar
     controller: searchController,
     style: TextStyle(color: Theme.of(context).iconTheme.color),
     decoration: kTextFieldDecoration.copyWith(hintText: "Search", prefixIcon: const Icon(Icons.search,), fillColor: Theme.of(context).canvasColor),
@@ -43,6 +50,8 @@ class _SearchScreenState extends State<SearchScreen> {
     ),
     ),
     ),
+
+    //list of places
     Expanded(
     child: StreamBuilder<QuerySnapshot>(
         stream: _firestore.collection('placeData').where("city",isEqualTo: session.city).orderBy("name", descending: false).snapshots(),
@@ -68,6 +77,8 @@ class _SearchScreenState extends State<SearchScreen> {
       return Padding(
           padding: const EdgeInsets.symmetric(vertical: 6),
           child: GestureDetector(
+
+            //when the user click on the place he/she will be transferred to the place details page
             onTap: (){
               push(context, PlaceDetailScreen(place: placeData?.elementAt(index).get("name"), imageURL: placeData?.elementAt(index).get(
                   "images")[0]));

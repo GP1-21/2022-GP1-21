cfbe2267c2a2a8be1a634c58865a7d7ed622d585
GP1-21 <113027700+GP1-21@users.noreply.github.com>
2023-04-14 20:42:47 +0300

    Update deletePlace_Card.dart

--- libAdmin/Widgets/deletePlace_Card.dart
+++ libAdmin/Widgets/deletePlace_Card.dart
@@ -4,6 +4,7 @@ import 'package:huna_ksa_admin/Widgets/rounded_Button.dart';
 import '../Components/common_Functions.dart';
 import '../Components/constants.dart';
 
+//will reserve the data from the delete_Screen page to delete a place
 Widget DeletePlaceCard(List imagePaths,String placeName,Function delete,BuildContext context)
 {
   return Container(
@@ -25,9 +26,9 @@ Widget DeletePlaceCard(List imagePaths,String placeName,Function delete,BuildCon
               child: SizedBox.fromSize(
                 //size: Size.fromWidth(height), // Image radius
                 child: Image.network(imagePaths[0], fit: BoxFit.fill,
-                ),
               ),
             ),
+          ),
           ),
         ),
         Positioned(left:120,top:45,child: Text(placeName,style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.black),)),
@@ -43,6 +44,7 @@ Widget DeletePlaceCard(List imagePaths,String placeName,Function delete,BuildCon
   );
 }
 
+//will reserve the data from the delete_Comment_Screen page to delete a comment
 Widget DeleteCommentCard(String username,String comment,String placeName,Function delete,BuildContext context)
 {
   return Container(
@@ -63,17 +65,17 @@ Widget DeleteCommentCard(String username,String comment,String placeName,Functio
             child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(username.trim(),style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.black),),
-                    SizedBox(height: 10,),
+              Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(username.trim(),style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.black),),
+                SizedBox(height: 10,),
 
-                    Text(comment,style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.black),),
-                  ],),
+                Text(comment,style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.black),),
+              ],),
                 deleteButton((){delete(username+placeName+comment);},context),
                 //RoundedButton(title: "Delete", color: kPinkColor, onPress: (){delete(username+placeName+comment);}, textColor: Colors.black,height: 20, horizontal: 0,width: 80,)
 
-              ],),
+            ],),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20,),
@@ -86,6 +88,7 @@ Widget DeleteCommentCard(String username,String comment,String placeName,Functio
   );
 }
 
+//will reserve the data from the reported_Comment_Screen page to delete a comment
 Widget DeleteReportedCommentCard(String username,String comment,String placeName,Function delete,Function deleteReport,BuildContext context,String id)
 {
   return Container(
@@ -136,4 +139,3 @@ Widget DeleteReportedCommentCard(String username,String comment,String placeName
       ),
     ),
   );
-}

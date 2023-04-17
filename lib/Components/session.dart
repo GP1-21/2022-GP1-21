b9257d128ed77ed2b45283b9c3d62b218d806d2f
shaden <shougalhazzani@gmail.com>
2023-04-17 06:58:16 +0300

    Update

--- lib/Components/session.dart
+++ lib/Components/session.dart
@@ -2,6 +2,8 @@ library my_prj.globals;
 
 String username = "",email="",password="",city="";
 List interests=[];
+List favourite=[];
+
 Map<String, dynamic> cityImage = {
-  "Al-Ula":"images/Al-ola.png",'Riyadh':"images/Riyadh.png",'Abha':"images/Abha.png",'Jeddah':"images/Jeddah.png",'Alkhobar':"images/Alkhobar.png"
+  "Al-Ula":"images/ALULA.jpg",'Riyadh':"images/Riyadh_.jpg",'Abha':"images/abha1.jpg",'Jeddah':"images/jeddah-city.jpg",'Alkhobar':"images/Khobar1.jpg"
 };

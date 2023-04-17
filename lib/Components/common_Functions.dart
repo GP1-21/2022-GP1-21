ca9162926ca575814262ebf9d3b5b6e8734724d8
shaden <shougalhazzani@gmail.com>
2023-04-17 06:43:35 +0300

    Update (add new functions)

--- lib/Components/common_Functions.dart
+++ lib/Components/common_Functions.dart
@@ -44,14 +44,38 @@ showAlertDialog(BuildContext context,Function yesFunction,String title,String te
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(title,style: TextStyle(color: Colors.black),),
-        content: Text(text),
-        backgroundColor: kBodyColor,
-        actions: [
-          noButton,
-          yesButton,
-        ],
+      return Container(
+        child: AlertDialog(
+
+          title: Text("Add your comment:",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 25),),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32.0))),
+          contentPadding: EdgeInsets.only(top: 10.0),
+          content: Container(
+              width:MediaQuery.of(context).size.width*.90,
+              height: 140.0,
+              decoration: new BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: new BorderRadius.all(new Radius.circular(32.0)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0,vertical: 10),
+                child: Center(child:
+                //Text(text,style: TextStyle(fontSize: 27),textAlign: TextAlign.center,)
+                TextField(
+                  controller: controller,
+                  decoration: kAddTextFieldDecoration,
+                  maxLines: 4,
+                ),
+                ),
+              )),
+
+
+          backgroundColor: kBackgroundColor,
+          actions: [
+            Align(alignment:Alignment.centerRight,child: yesButton),
+          ],
+        ),
       );
     },
   );
@@ -69,9 +93,11 @@ myNotifier(BuildContext context,String message,{String label="",required Functio
   ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
     backgroundColor: const Color(0xFF616161),
-    duration: Duration(seconds: duration),
-    content: Text(message,style: TextStyle(color: Colors.white,),textAlign: TextAlign.center,),
-    behavior: SnackBarBehavior.fixed,
+          duration: Duration(seconds: duration), behavior: SnackBarBehavior.floating,
+          margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height*.50),
+          dismissDirection: DismissDirection.none,
+          content: Text(message,style: TextStyle(color: Colors.white,),textAlign: TextAlign.center,),
+          //behavior: SnackBarBehavior.fixed,
     action: label!=""?SnackBarAction(
       label: label,
       disabledTextColor: Colors.white,
@@ -83,33 +109,86 @@ myNotifier(BuildContext context,String message,{String label="",required Functio
     ): null
     ));
 }
-// class DrawTriangleShape extends CustomPainter {
-//
-//   late Paint painter;
-//
-//   DrawTriangleShape() {
-//
-//     painter = Paint()
-//       ..color = kPrimaryColor
-//       ..style = PaintingStyle.fill;
-//
-//   }
-//
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//
-//     var path = Path();
-//
-//     path.moveTo(size.width/2, 180);
-//     path.lineTo(-40, size.height-120);
-//     path.lineTo(size.height+40, size.width-120);
-//     path.close();
-//
-//     canvas.drawPath(path, painter);
-//   }
-//
-//   @override
-//   bool shouldRepaint(CustomPainter oldDelegate) {
-//     return false;
-//   }
-// }
+Widget smallButton(String text,Function function,{
+  Color color=const Color(0xFFD6D6D6),
+  double height=30,
+  double width=85,
+  double fontSize=12}) {
+  return GestureDetector(
+    onTap: (){
+      function();
+    },
+    child: Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(color: color,borderRadius: BorderRadius.circular(25)),
+      child:Center(child: Text
+        (text, style: TextStyle(color: Colors.black,fontSize: fontSize,fontWeight: FontWeight.bold),)
+      ),
+    ),
+  );
+}
+Widget deleteButton(Function function,) {
+  return GestureDetector(
+    onTap: (){
+      function();
+    },
+    child: ImageIcon(AssetImage('images/delete.png'),size: 27,color: Colors.black,),
+  );
+}
+Widget iconButton(Function function,Color color,String imagePath) {
+  return GestureDetector(
+    onTap: (){
+      function();
+    },
+    child: Container(
+        height: 40,width: 50,
+        decoration:BoxDecoration(color: color,borderRadius: BorderRadius.circular(10)),child: Center(child: ImageIcon(AssetImage(imagePath),size: 27,color: Colors.black,))),
+  );
+}
+Widget categoryCard(String text,Color color,Color borderColor,double width,Function function) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 7),
+    child: GestureDetector(
+      onTap: (){
+        function(text);
+      },
+      child: Container(
+        height: 20,width: width,
+        decoration: BoxDecoration(color: color,borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: borderColor)),
+        child:Center(child: Text(text, style: const TextStyle(color: Colors.black,fontSize: 14,fontWeight: FontWeight.bold),)),
+      ),
+    ),
+  );
+}
+
+Widget commentCard(String text,String username,String id,Function function,BuildContext context) {
+  return Column(crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(left: 30.0),
+        child: Text(username.trim(), style: const TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
+      ),
+
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ImageIcon(AssetImage('images/profile.png'),size: 27,color: Colors.black,),
+              SizedBox(width: 5,),
+              Text(text, style: const TextStyle(color: Colors.black,fontSize: 14,fontWeight: FontWeight.bold),),
+
+            ],
+          ),
+          GestureDetector(onTap:(){
+            function(id,text);
+          },child: Icon(Icons.info_outline,size:
+          27,))
+        ],
+      ),
+      Divider(thickness: 1,)
+
+    ],
+  );
+}

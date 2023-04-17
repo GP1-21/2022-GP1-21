007ca4c69fef4706fb9429c3de6ca9ea702f51ae
shaden <shougalhazzani@gmail.com>
2023-04-17 06:47:50 +0300

    Update

--- lib/Widgets/city_Card.dart
+++ lib/Widgets/city_Card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:huna_ksa/Components/constants.dart';
 class CityCard extends StatefulWidget {
 CityCard({required this.imagePath,required this.cityName});
 
@@ -14,29 +13,21 @@ class _CityCardState extends State<CityCard> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top:15),
-      child: Container(
-        height: 120,
-        width: MediaQuery.of(context).size.width*.70,
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(25)
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(height: 90,
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20), // Image border
-                child: SizedBox.fromSize(
-                  //size: Size.fromWidth(height), // Image radius
-                  child: Image(image:AssetImage(widget.imagePath,), fit: BoxFit.cover),
-                ),
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          SizedBox(height: 130,
+            width: double.infinity,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // Image border
+              child: SizedBox.fromSize(
+                //size: Size.fromWidth(height), // Image radius
+                child: Image(image:AssetImage(widget.imagePath,), fit: BoxFit.cover),
               ),
             ),
-            Text(widget.cityName,style: const TextStyle(fontSize: 22,fontWeight: FontWeight.bold),)
-          ],
-        ),
+          ),
+          Text(widget.cityName,style: const TextStyle(fontSize: 32,fontWeight: FontWeight.bold,color: Colors.white),)
+        ],
       ),
     );
   }
